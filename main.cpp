a2233a5462ce6d15d35470ca5e048b40804ede51
Sachin Saini <iamsainisachin@gmail.com>
2019-05-26 01:04:53 +0530

    initial commit

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "ncurses.h"
+
+using namespace std;
+
+int main(){
+	int ch;
+	// init curses mode
+	initscr();
+	// disable line buffering
+	raw();
+	// enable key support
+	keypad(stdscr, TRUE);
+	// no echoing
+	noecho();
+	printw("type something...");
+	ch = getch();
+
+	if(ch == KEY_F(1)){
+		printw("key f1 pressed");
+	}else{
+		printw("the pressed key is : ");
+		attron(A_BOLD);
+		printw("%c", ch);
+		attroff(A_BOLD);
+	}
+	refresh();
+	getch();
+	endwin();
+	return 0;
+}
---
+++ makefile
@@ -0,0 +1,4 @@
+CXX = g++
+
+main: main.cpp
+	$(CXX) main.cpp -lncurses -o ncurses
